a398602f5fc77de09e203a1563ed8d1773aaaefb
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-10-23 09:29:51 -0400

    Add project files.

---
+++ 2Parcial_Aplicada2.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2Parcial_Aplicada2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
